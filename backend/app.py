f6420270f8e5a83a24ea7b67754996bc0af5e80f
mano37 <kjhwanid@gmail.com>
2025-01-09 10:11:37 +0900

    CI backend test

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# check health
 @app.route('/api/health', methods=['GET'])
 def health_check():
     return jsonify({"status": "ok"}), 200
